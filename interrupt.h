b21e33deb12f809a1781d6a6ad34414193417e32
Vitor Finotti <vfinotti@ncc.unesp.br>
2019-01-31 00:38:27 -0200

    Adding initial files for Cortex-M0 blinky project

--- interrupt.h
+++ interrupt.h
@@ -8,16 +8,20 @@
 void interrupt_reset (void);
 void interrupt_nmi (void);
 void interrupt_hard_fault (void);
-void interrupt_memmanage (void);
-void interrupt_bus_fault (void);
-void interrupt_usage_fault (void);
+/* void interrupt_memmanage (void); */
+/* void interrupt_bus_fault (void); */
+/* void interrupt_usage_fault (void); */
 void interrupt_reserved_0 (void);
 void interrupt_reserved_1 (void);
 void interrupt_reserved_2 (void);
 void interrupt_reserved_3 (void);
+void interrupt_reserved_4 (void);
+void interrupt_reserved_5 (void);
+void interrupt_reserved_6 (void);
 void interrupt_svcall (void);
 void interrupt_debug (void);
-void interrupt_reserved_4 (void);
+void interrupt_reserved_7 (void);
+void interrupt_reserved_8 (void);
 void interrupt_pendsv (void);
 void interrupt_systick (void);
 
--- vector_table.c
+++ vector_table.c
@@ -12,16 +12,20 @@ void *vector_table[] __attribute__((section(".vector_table"))) =
     (void *) interrupt_reset,
     (void *) interrupt_nmi,
     (void *) interrupt_hard_fault,
-    (void *) interrupt_memmanage,
-    (void *) interrupt_bus_fault,
-    (void *) interrupt_usage_fault,
+    /* (void *) interrupt_memmanage, */
+    /* (void *) interrupt_bus_fault, */
+    /* (void *) interrupt_usage_fault, */
     (void *) interrupt_reserved_0,
     (void *) interrupt_reserved_1,
     (void *) interrupt_reserved_2,
     (void *) interrupt_reserved_3,
+    (void *) interrupt_reserved_4,
+    (void *) interrupt_reserved_5,
+    (void *) interrupt_reserved_6,
     (void *) interrupt_svcall,
     (void *) interrupt_debug,
-    (void *) interrupt_reserved_4,
+    (void *) interrupt_reserved_7,
+    (void *) interrupt_reserved_8,
     (void *) interrupt_pendsv,
     (void *) interrupt_systick,
 };
